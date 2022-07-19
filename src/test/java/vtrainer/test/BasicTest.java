0cb59e8d0f51f7788c379a038081ba25c1582d3c
krueger <robert.m.krueger.de@gmail.com>
2022-07-19 09:31:23 +0200

    Formatting

--- src/test/java/vtrainer/test/BasicTest.java
+++ src/test/java/vtrainer/test/BasicTest.java
@@ -5,42 +5,41 @@ import junit.framework.TestCase;
 import vtrainer.Dictionary;
 import vtrainer.DictionaryEntry;
 
-public class BasicTest extends TestCase{
-    
+public class BasicTest extends TestCase {
+
     private Dictionary dictionary = null;
-    
-    public BasicTest(String name){
-	super(name);
+
+    public BasicTest(String name) {
+        super(name);
     }
-    
-    protected void setUp(){
-	dictionary = new Dictionary();
-	DictionaryEntry de = new DictionaryEntry();
-	de.setName("problema");
-	de.getTranslations().add("problem");
-	dictionary.addEntry(de);
-	
-	de = new DictionaryEntry();
-	de.setName("venir");
-	de.getTranslations().add("kommen");
-	de.getTranslations().add("gehen");
-	dictionary.addEntry(de);
+
+    protected void setUp() {
+        dictionary = new Dictionary();
+        DictionaryEntry de = new DictionaryEntry();
+        de.setName("problema");
+        de.getTranslations().add("problem");
+        dictionary.addEntry(de);
+
+        de = new DictionaryEntry();
+        de.setName("venir");
+        de.getTranslations().add("kommen");
+        de.getTranslations().add("gehen");
+        dictionary.addEntry(de);
     }
-    
-    protected void tearDown(){
-	dictionary = null;
+
+    protected void tearDown() {
+        dictionary = null;
     }
-    
-    public void testDataStructure() throws Exception{
-	DictionaryEntry de = new DictionaryEntry();
-	Assert.assertNull("new Dictionary entry has non-null ID", de.getID());
-	
-	de.setName("mano");
-	de.getTranslations().add("hand");
-	
-	Assert.assertTrue("correct translation doesn't work", de.isTranslation("Hand"));
-	Assert.assertTrue("incorrect translation does work", !de.isTranslation("bein"));
-	
+
+    public void testDataStructure() throws Exception {
+        DictionaryEntry de = new DictionaryEntry();
+        Assert.assertNull("new Dictionary entry has non-null ID", de.getID());
+
+        de.setName("mano");
+        de.getTranslations().add("hand");
+
+        Assert.assertTrue("correct translation doesn't work", de.isTranslation("Hand"));
+        Assert.assertTrue("incorrect translation does work", !de.isTranslation("bein"));
     }
 
 }
